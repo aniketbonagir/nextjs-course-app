89eed7b4d101c817c9346f4af5e88d22da014ec5
Aniket Bonagir <abonagir@qualys.com>
2022-09-19 14:46:29 +0530

    Setup and routing
    - Catch all routes
    - Dynamic routes
    - Setting up page routes

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const Page = () => (
+    <div>
+        <h1>
+            Index Page
+        </h1>
+        <Link href="/notes">
+            Link
+        </Link>
+    </div>
+);
+
+export default Page;
